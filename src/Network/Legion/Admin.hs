207858aa9f2e402be211d00f622756747f7ef2d7
Rick Owens <rick@owenssoftware.com>
2016-07-21 17:56:05 -0500

    Improvements to the admin web service. ...
    - Added better exception logging.
    - Added better request logging.
    - Set the `Server:` response header.

--- src/Network/Legion/Admin.hs
+++ src/Network/Legion/Admin.hs
@@ -7,6 +7,7 @@ module Network.Legion.Admin (
   runAdmin,
 ) where
 
+import Canteven.HTTP (requestLogging, logExceptionsAndContinue)
 import Control.Concurrent (forkIO, newChan, newEmptyMVar, writeChan,
   putMVar, takeMVar, Chan)
 import Control.Monad (void)
@@ -14,17 +15,24 @@ import Control.Monad.Logger (askLoggerIO, runLoggingT)
 import Control.Monad.Trans.Class (lift)
 import Data.Conduit (Source)
 import Data.Default.Class (def)
+import Data.Text.Encoding (encodeUtf8)
 import Data.Text.Lazy (Text, pack)
+import Data.Version (showVersion)
 import Network.Legion.Application (LegionConstraints)
 import Network.Legion.Conduit (chanToSource)
 import Network.Legion.LIO (LIO)
 import Network.Legion.PartitionKey (PartitionKey(K))
 import Network.Legion.StateMachine (AdminMessage(GetState, GetPart))
+import Network.Wai (Middleware, modifyResponse)
 import Network.Wai.Handler.Warp (HostPreference, defaultSettings, Port,
   setHost, setPort)
+import Network.Wai.Middleware.AddHeaders (addHeaders)
+import Network.Wai.Middleware.StripHeaders (stripHeader)
+import Paths_legion (version)
 import Web.Scotty.Resource.Trans (resource, get)
 import Web.Scotty.Trans (Options, scottyOptsT, settings, ScottyT, text,
-  ActionT, param)
+  ActionT, param, middleware)
+import qualified Data.Text as T
 
 {- |
   Start the admin service in a background thread.
@@ -40,6 +48,11 @@ runAdmin addr host = do
     let
       website :: ScottyT Text LIO ()
       website = do
+        middleware
+          $ requestLogging logging
+          . setServer
+          . logExceptionsAndContinue logging
+
         resource "/clusterstate" $
           get $ do
             val <- send chan GetState
@@ -73,4 +86,25 @@ options port host = def {
       $ defaultSettings
   }
 
+
+setServer :: Middleware
+setServer = addServerHeader . stripServerHeader
+  where
+    {- |
+      Strip the server header
+    -}
+    stripServerHeader :: Middleware
+    stripServerHeader = modifyResponse (stripHeader "Server") 
+
+    {- |
+      Add our own server header.
+    -}
+    addServerHeader :: Middleware
+    addServerHeader = addHeaders [("Server", serverValue)]
+
+    {- |
+      The value of the @Server:@ header.
+    -}
+    serverValue =
+      encodeUtf8 (T.pack ("legion-admin/" ++ showVersion version))
 
